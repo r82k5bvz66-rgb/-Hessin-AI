bd745fa2b12aeeb6398892accfcf5e9e0c1174e0
r82k5bvz66-rgb <r82k5bvz66@privaterelay.appleid.com>
2026-09-10 15:44:19 +0300

    chore: bump service worker cache to v2242

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "hessin-ai-v2241";
+const CACHE = "hessin-ai-v2242";
 const ASSETS = ["/", "/index.html", "/style.css", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", (event) => {
