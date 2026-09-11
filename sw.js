65c3b7646bd65a88ed384fbc1007e264fe165c2f
r82k5bvz66-rgb <r82k5bvz66@privaterelay.appleid.com>
2026-09-11 07:22:33 +0300

    chore: SW cache hessin-ai-v2261

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "hessin-ai-v2260";
+const CACHE = "hessin-ai-v2261";
 const ASSETS = ["/", "/index.html", "/style.css", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", (event) => {
